6d0e058eec16ec128599c5cb6ca18b795d0a2c8d
nirui <nirui@haizhi.com>
2015-01-01 18:36:38 +0800

    login part

--- app/scripts/controllers/loginCtrl.js
+++ app/scripts/controllers/loginCtrl.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('angularWorkApp')
-    .controller('LoginCtrl',['$scope',function($scope){
+    .controller('LoginCtrl',['$scope','$modal',function($scope,$modal){
+        var fn = $scope.fn = {};
 
+        fn.login = function() {
+            var loginModal = $modal({
+
+            });
+        }
     }]);
